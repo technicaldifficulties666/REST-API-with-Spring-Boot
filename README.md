de59c9134c93e88fecf4fceb2ab4f1f9f2d2b921
Subaig Bindra <subaig_singh_bindra@sfu.ca>
2024-03-08 00:27:26 -0800

    add - implemented GET, POST, PUT

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# REST-API-with-Spring-Boot
+# REST-API-with-Spring-Boot README
+## Overview
 REST API with Spring Boot, utilizing Spring Security to authenticate and authorize the app. 
+
+
+## Getting Started
+
+NOTE: This guide assumes windows installation
+
+To get started, you'll need to have  Java JDK version 17 and Gradle 8.6 installed on your machine 
+
+Follow this guide to install and switch between multiple JDKs in windows: https://www.happycoders.eu/java/how-to-switch-multiple-java-versions-windows/ 
+
+Follow guide on https://spring.io/quickstart for starting a new Spring Boot project
+
+Optional: (if code doesn't compile)
+add gradle.properties file in the project directory and add the line:
+```bash
+org.gradle.java.home =  C:/Program Files/Java/jdk-17 ##<Path to the JDK you want to use for your project (JDK 17)>
+```
+
+For building and running tests: In the terminal, go to project directory and run command
+```bash 
+./gradlew test
+```
---
+++ src/main/java/example/cashcard/CashCardController.java
@@ -0,0 +1,77 @@
+package example.cashcard;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.*;
+import java.security.Principal;
+
+
+@RestController
+@RequestMapping("/cashcards")
+class CashCardController {
+  private final CashCardRepository cashCardRepository;
+
+  private CashCardController(CashCardRepository cashCardRepository) {
+    this.cashCardRepository = cashCardRepository;
+
+  }
+
+  @GetMapping("/{requestedId}") // Read one (GET)
+  private ResponseEntity<CashCard> findById(@PathVariable Long requestedId, Principal principal) {
+    // Optional<CashCard> cashCardOptional =
+    // cashCardRepository.findById(requestedId);
+    CashCard cashCard = findCashCard(requestedId, principal);
+    if (cashCard != null) {
+      return ResponseEntity.ok(cashCard);
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  @PostMapping // Add one (POST)
+  private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb, Principal principal) {
+    CashCard cashCardWithOwner = new CashCard(null, newCashCardRequest.amount(), principal.getName());
+    CashCard savedCashCard = cashCardRepository.save(cashCardWithOwner);
+    URI locationOfNewCashCard = ucb
+        .path("cashcards/{id}")
+        .buildAndExpand(savedCashCard.id())
+        .toUri();
+    return ResponseEntity.created(locationOfNewCashCard).build();
+  }
+
+  @GetMapping() // Get all
+  private ResponseEntity<Iterable<CashCard>> findAll(Pageable pageable, Principal principal) {
+    Page<CashCard> page = cashCardRepository.findByOwner(principal.getName(),//cashCardRepository.findAll(
+        PageRequest.of(
+            pageable.getPageNumber(),
+            pageable.getPageSize(),
+            pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))));
+    return ResponseEntity.ok(page.getContent());
+    // return ResponseEntity.ok(cashCardRepository.findAll());
+  }
+
+  @PutMapping("/{requestedId}")
+  private ResponseEntity<Void> putCashCard(@PathVariable Long requestedId, @RequestBody CashCard cashCardUpdate,
+      Principal principal) {
+    CashCard cashCard = findCashCard(requestedId, principal);
+    if (cashCard != null) {
+      CashCard updatedCashCard = new CashCard(cashCard.id(), cashCardUpdate.amount(), principal.getName());
+      cashCardRepository.save(updatedCashCard);
+      // just return 204 NO CONTENT for now.
+      return ResponseEntity.noContent().build();
+    }
+    return ResponseEntity.notFound().build();
+  }
+
+  private CashCard findCashCard(Long requestedId, Principal principal) {
+    return cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
+  }
+  
+}
